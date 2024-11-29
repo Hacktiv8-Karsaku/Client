d8841c90d2ec8a353b656740da7617a6c13b0b54
AbdulRidhoRamadhan <abdulridhoramadhan.it@gmail.com>
2024-11-29 09:40:13 +0700

    feat: wiring login & register from backend

--- Screens/Login.jsx
+++ Screens/Login.jsx
@@ -1,69 +1,138 @@
-import React from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { gql, useMutation } from "@apollo/client";
+import { useNavigation } from "@react-navigation/native";
+import { useContext, useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Alert,
+  Keyboard,
+  TouchableOpacity,
+  ActivityIndicator,
+  TouchableWithoutFeedback,
+} from "react-native";
+import * as SecureStore from "expo-secure-store";
+import { AuthContext } from "../context/AuthContext";
 
-const LoginPage = ({ navigation }) => {
+const LOGIN = gql`
+  mutation Login($username: String, $password: String) {
+    login(username: $username, password: $password) {
+      access_token
+      userId
+      username
+    }
+  }
+`;
+
+export default function Login() {
+  const [login, { loading }] = useMutation(LOGIN);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { setIsSignedIn } = useContext(AuthContext);
+  const navigation = useNavigation();
+
+  const handleLogin = async () => {
+    try {
+      const result = await login({
+        variables: {
+          username: username,
+          password: password,
+        },
+      });
+      await SecureStore.setItemAsync(
+        "access_token",
+        result.data.login.access_token
+      );
+      await SecureStore.setItemAsync("user_id", result.data.login.userId);
+      setIsSignedIn(true);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Login Error", error.message);
+    } finally {
+      Keyboard.dismiss();
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <TextInput style={styles.input} placeholder="Email" keyboardType="email-address" />
-      <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} />
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
-      <Text style={styles.registerPrompt}>
-        Don't have any account?{' '}
-        <Text style={styles.registerLink} onPress={() => navigation.navigate('Register')}>
-          Register here
+    <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <TextInput
+          onChangeText={setUsername}
+          value={username}
+          placeholder="Username"
+          style={styles.input}
+        />
+        <TextInput
+          onChangeText={setPassword}
+          value={password}
+          placeholder="Password"
+          style={styles.input}
+          secureTextEntry={true}
+        />
+        {!loading ? (
+          <TouchableOpacity style={styles.button} onPress={handleLogin}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+        ) : (
+          <ActivityIndicator size="large" color="#1877F2" />
+        )}
+        <Text style={styles.registerPrompt}>
+          Don't have any account?
+          <Text
+            style={styles.registerLink}
+            onPress={() => navigation.navigate("Register")}
+          >
+            Register here
+          </Text>
         </Text>
-      </Text>
-    </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFF5F3',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFF5F3",
   },
   title: {
     fontSize: 32,
-    fontWeight: 'bold',
-    color: '#FF9A8A',
+    fontWeight: "bold",
+    color: "#FF9A8A",
     marginBottom: 20,
   },
   input: {
-    width: '80%',
+    width: "80%",
     padding: 10,
     marginVertical: 10,
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: '#FF9A8A',
+    borderColor: "#FF9A8A",
   },
   button: {
     marginTop: 20,
-    width: '80%',
-    backgroundColor: '#FF9A8A',
+    width: "80%",
+    backgroundColor: "#FF9A8A",
     padding: 15,
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   buttonText: {
-    color: '#FFF',
-    fontWeight: 'bold',
+    color: "#FFF",
+    fontWeight: "bold",
     fontSize: 16,
   },
   registerPrompt: {
     marginTop: 20,
     fontSize: 14,
-    color: '#333',
+    color: "#333",
   },
   registerLink: {
-    color: '#FF9A8A',
-    fontWeight: 'bold',
+    color: "#FF9A8A",
+    fontWeight: "bold",
   },
 });
-
-export default LoginPage;
